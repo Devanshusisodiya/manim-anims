c32984ca276fd46acec7baf719c39e19b245cc8d
devanshu <devanshukumar45@gmail.com>
2022-03-09 19:03:29 +0530

    coord geo added

--- CoordinateGeo/coordGeo.py
+++ CoordinateGeo/coordGeo.py
@@ -1,92 +1,126 @@
 from os import replace
+from turtle import down
 from manim import *
 from numpy.core.numeric import base_repr
 
-class DistanceFormula(Scene):
+class DF(Scene):
     def construct(self):
 
-        #-------------------------------------------------
-        intro_text = MathTex("\\text{Coordinate Geometry}", color=BLUE).shift(ORIGIN)
-        self.play(Write(intro_text))
-        self.play(FadeOut(intro_text))
+        # INTRO -----------------------------------------------------------------------
+        intro = MathTex("\\text{Coordinate Geometry}", color=BLUE).shift(ORIGIN)
+        self.play(Write(intro))
+        self.play(FadeOut(intro))
 
-        form1 = MathTex("\\text{Distance Formula}").shift(2*UP+4*RIGHT)
-        form2 = MathTex("\\text{Section Formula}").next_to(form1, DOWN)
-        self.play(Write(VGroup(form1, form2)))
-        self.play(FadeOut(form2))
+        # SUB SCENE 1 ------------------------------------------------------------------
+        # FORMULAS
+        distForm = MathTex("\\text{Distance Formula}").shift(2*UP+4*RIGHT)
+        secForm = MathTex("\\text{Section Formula}").next_to(distForm, DOWN)
+        self.play(Write(VGroup(distForm, secForm)))
+        self.play(FadeOut(secForm))
         
-        # GRAPHING
+        # GRAPHING 
         axes = Axes(
             x_range=(-1, 9),
             x_length=7,
             y_range=(-1, 9),
         ).shift(2*LEFT)
-        self.play(Create(axes))
 
-
-        point_A = Dot(axes.c2p(2,2))
-        point_A_label = MathTex("(","x_1",",","y_1",")","A").scale(0.7).next_to(point_A, UP+0.001*LEFT)
-        point_B = Dot(axes.c2p(7,7))
-        point_B_label = MathTex("(","x_2",",","y_2",")","B").scale(0.7).next_to(point_B, UP+RIGHT)
         self.play(
-            Write(point_A_label),
-            Write(point_B_label),
-            Create(point_A),
-            Create(point_B)
+            Create(axes),
+            distForm.animate.scale(0.7).shift(UR))
+
+        # USING MATHTEX BECAUSE ANIM REQUIRES SO
+        # SOME DEGREE OF MANIPULATION REQUIRED
+        # THESE ARE THE POINTS ON THE GRAPH
+        A = Dot(axes.c2p(2,2))
+        ALabel = MathTex("(","x_1",",","y_1",")","A").scale(0.7).next_to(A, UP + 0.01*LEFT)
+        B = Dot(axes.c2p(7,7))
+        BLabel = MathTex("(","x_2",",","y_2",")","B").scale(0.7).next_to(B, UP)
+        C = Dot(axes.c2p(7,2))
+        CLabel = Tex("$C$").scale(0.7).next_to(C, RIGHT)
+        self.play(
+            Write(ALabel),
+            Write(BLabel),
+            Create(A),
+            Create(B)
         )
         
+        # LINES CONNECTING AXES AND POINTS A, B
         y1_line = Line(start=axes.c2p(2,0), end=axes.c2p(2,2), color=BLUE) #x1
         x1_line = Line(start=axes.c2p(0,2), end=axes.c2p(2,2), color=BLUE) #y1
         y2_line = Line(start=axes.c2p(7,0), end=axes.c2p(7,7), color=BLUE) #x2
         x2_line = Line(start=axes.c2p(0,7), end=axes.c2p(7,7), color=BLUE) #y2
-
 
         x1_copy = x1_line.copy().shift(1.5*DOWN)
         x2_copy = x2_line.copy().shift(4.35*DOWN)
         y2_copy = y2_line.copy().next_to(y2_line, RIGHT)
         y1_copy = y1_line.copy().shift(4*RIGHT)
         
-
-
+        # SOME LABELS
         distance = Line(start=axes.c2p(2,2), end=axes.c2p(7,7))
-        dist_label = MathTex("d").next_to(distance.get_center(), UP)
+        distanceLabel = MathTex("d").next_to(distance.get_center(), UP)
 
         base = Line(start=axes.c2p(2,2), end=axes.c2p(7,2), color=ORANGE)
-        base_label = MathTex("x_2","-","x_1").scale(0.7).next_to(base.get_center(), DOWN)
+        baseLabel = MathTex("x_2","-","x_1").scale(0.7).next_to(base.get_center(), DOWN)
         perp = Line(start=axes.c2p(7,2), end=axes.c2p(7,7), color=ORANGE)
-        perp_label = MathTex("y_2","-","y_1").scale(0.7).next_to(perp.get_center(), RIGHT)
+        perpLabel = MathTex("y_2","-","y_1").scale(0.7).next_to(perp.get_center(), RIGHT)
 
         self.play(
             Create(distance),
-            Write(dist_label)
-        )
-        self.play(
-            Indicate(point_A_label[1]),
-            ReplacementTransform(point_A_label[1].copy(), x1_line))
-        self.play(
-            Indicate(point_B_label[1]),
-            ReplacementTransform(point_B_label[1].copy(), x2_line))
-        self.play(
-            Indicate(point_A_label[3]),
-            ReplacementTransform(point_A_label[3].copy(), y1_line))
-        self.play(
-            Indicate(point_B_label[3]),
-            ReplacementTransform(point_B_label[3].copy(), y2_line))
+            Write(distanceLabel))
 
+        # ANIM FOR CREATING LINES
+        for i, j in zip([ALabel, BLabel], [x1_line, x2_line]):
+            self.play(
+                Indicate(i[1]),
+                ReplacementTransform(i[1].copy(), j))
+        for i, j in zip([ALabel, BLabel], [y1_line, y2_line]):
+            self.play(
+                Indicate(i[3]),
+                ReplacementTransform(i[3].copy(), j))
 
-        self.play(ReplacementTransform(x2_line.copy(), x2_copy))
-        self.play(ReplacementTransform(x1_line.copy(), x1_copy))
-         
+        # SHOWING DIFFERENCE BETWEEN LINES
+        # ONLY SOME OF THIS IS DYNAMIC
 
+        # CREATING BASE LINE
+        for i, j in zip([x2_line, x1_line],[x2_copy, x1_copy]):
+            self.play(ReplacementTransform(i.copy(), j))
         self.play(
             Transform(VGroup(x1_copy, x2_copy), base),
-            Write(base_label)
+            Write(baseLabel)
         )
-        
+
+        # CREATING PERPENDICULAR
+        for i, j in zip([y2_line, y1_line],[y2_copy, y1_copy]):
+            self.play(ReplacementTransform(i.copy(), j))
         self.play(
             Transform(VGroup(y1_copy, y2_copy), perp),
-            Write(perp_label)
+            Write(perpLabel)
         )
+
+        # FINALLY DISPLAYING C
+        self.play(
+            Write(C),
+            Write(CLabel))
+        #--------------------------------------------------------------------------------------
+        # SUB SCENE 2--------------------------------------------------------------------------
+        # WRITING THE FORMULA
+        form1 = Tex("Considering $\\triangle$ABC").scale(0.7).shift(4*RIGHT)
+        form2 = Tex("$AB^2 = BC^2 + AC^2$").scale(0.7).next_to(form1, DOWN, aligned_edge=LEFT)
+        form3 = Tex("$d^2 =$", "$(x_2 - x_1)^2$", "$+$", "$(y_2 - y_1)^2$").scale(0.7).next_to(form2, DOWN, aligned_edge=LEFT)
+        form4 = Tex("$d =$", "$\sqrt{(x_2 - x_1)^2 + (y_2 - y_1)^2}$").scale(0.7).next_to(form3, DOWN, aligned_edge=LEFT)
+        
+        formula = VGroup(form1, form2, form3, form4)
+
+        for i in range(2):
+            self.play(Write(formula[i]))
+        # SOME TRICKS UP ME SLEEVE, HEHE
+        self.play(Write(form3[0]))
+        self.play(ReplacementTransform(baseLabel.copy(), form3[1]))
+        self.play(Write(form3[2]))
+        self.play(ReplacementTransform(perpLabel.copy(), form3[-1]))
+        #
+        self.play(Write(formula[-1]))
 
         self.wait(1)
         # FINAL WAIT TIME
